3ac0a468f8f1202f107aad0a76511a98111e43c9
Zach Sherman <zach@anemon.es>
2020-04-14 11:12:45 -0700

    add simple input component

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,27 @@
 <script>
+  import Question from './Question.svelte'
   import { store } from './stores.js'
+  let questionText = ''
+  const handleSubmit = () => {
+    console.log('posting question: ', questionText)
+  }
+  const resetQuestion = () => {
+    questionText = ''
+  }
 </script>
-<main> </main>
+<main>
+  <form on:submit|preventDefault="{handleSubmit}">
+    <textarea bind:value="{questionText}"></textarea>
+    <div>
+      <button type="submit">Submit</button>
+      <button on:click="{resetQuestion}">Cancel</button>
+    </div>
+  </form>
+  <code>{questionText}</code>
+  <Question date={'01/01/01'} asker={"Zach"} title={'How do I get a job'}></Question>
+</main>
+<style>
+  main {
+    margin: var(--s2);
+  }
+</style>
