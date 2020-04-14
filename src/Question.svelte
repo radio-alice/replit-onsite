29a2cb2a19514bb4495b23b98ee7fa81af52bb0a
Zach Sherman <zach@anemon.es>
2020-04-14 12:55:42 -0700

    add more question metadata

--- src/Question.svelte
+++ src/Question.svelte
@@ -1,33 +1,45 @@
 <script>
-  export let body, id, title
-  // numReplies,
-  // numUpvotes,
-  // acceptedAnswer,
-  // asker
+  export let body, id, title, voteCount, accepted, createdAt
 </script>
 <div class="question stack">
-  <div class="titleRow">
-    <!-- <p class="asker">{asker}</p> -->
+  <div class="row">
     <h3 class="title">{title}</h3>
-    <p>{id}</p>
+    <p>{createdAt.substring(0,10)}</p>
   </div>
   <p class="body">{body}</p>
-  <a href="https://search-api.moudy.repl.co/" class="openQuestion"
-    >Open full question thread →</a
-  >
+  <div class="row">
+    <a href="https://search-api.moudy.repl.co/" class="openQuestion"
+      >Open full question thread →</a
+    >
+    {#if accepted}
+    <p class="accepted">✓ Answered!</p>
+    {/if}
+    <p class="voteCount">up-votes: {voteCount}</p>
+  </div>
 </div>
 <style>
   .question {
     background-color: var(--light-grey);
     padding: var(--s0);
   }
-  .titleRow {
+  .row {
     display: flex;
     align-items: center;
   }
+  .row > * + * {
+    margin-left: var(--s0);
+  }
+  .accepted {
+    background-color: var(--green);
+    /* line-height: var(--s1); */
+    padding: var(--s-3);
+    white-space: nowrap;
+  }
   .title {
     flex: auto;
-    /* text-align: center; */
+  }
+  .voteCount {
+    font-style: italic;
   }
   .openQuestion {
     background-color: var(--blue);
