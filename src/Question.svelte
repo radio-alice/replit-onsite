1a633179963123497c43980ced968b8e2d51619b
Zach Sherman <zach@anemon.es>
2020-04-14 15:32:59 -0700

    make possible questions tabbable

--- src/Question.svelte
+++ src/Question.svelte
@@ -7,8 +7,11 @@
     keyTerms = new Set(),
     numMatches = 0,
     accepted = false
+
+  const focus = ({ target }) =>
+    target.scrollIntoView({ behavior: 'smooth', block: 'center' })
 </script>
-<div class="question stack">
+<div class="question stack" on:focus="{focus}" tabindex="0">
   <!-- {#if numMatches}
   <p>keyterms: {numMatches}</p>
   {/if} -->
@@ -30,6 +33,10 @@
     background-color: var(--light-grey);
     padding: var(--s0);
     max-width: var(--measure);
+  }
+  .question:focus {
+    outline: none;
+    box-shadow: 0 0 var(--s0) var(--s0) var(--pink);
   }
   .flush {
     margin: var(--s0) calc(-1 * var(--s0)) calc(-1 * var(--s0))
